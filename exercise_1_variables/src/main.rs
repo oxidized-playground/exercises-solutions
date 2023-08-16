72ae7f94f92f05aed3e739ff25b210e6c68979f6
Koen Braham <koen.braham@alten.nl>
2023-08-16 17:33:35 +0200

    Add exercise 1

---
+++ exercise_1_variables/src/main.rs
@@ -0,0 +1,16 @@
+/**
+ * Welcome to the ALTEN rust playground. In this exercise you will create a new variable and print its content.
+ */
+fn main() {
+    // Use cargo run to build and start the exercise
+
+    let x = 10;
+    println!("Hello, world! X: {}, Y: {}",  x, y);
+
+    // Modify x here
+    println!("Hello, world! X: {}, Y: {}",  x, y);
+
+
+    // Swap x and y here
+    println!("Hello, world! X: {}, Y: {}",  x, y);
+}
